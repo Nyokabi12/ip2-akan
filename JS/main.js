480f86154f5fbd7b3a7cad1e987d53b89cc8a159
Nyokabi12 <sylvianyokabi536@gmail.com>
2019-09-26 10:08:55 +0300

    addindex.html

--- JS/main.js
+++ JS/main.js
@@ -1,77 +1,52 @@
-<script>
-     function showName(){
+var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
-     var sex = document.querySelector('input[name="gender"]:checked').value;
-     var dbirth = document.getElementById("dob").value;
+var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-     var birthd = dbirth.split(" ");
-     var mms = birthd[0];
-     var dds = birthd[1];
-     var yys = birthd[2];
-     var mm = parseInt(mms);
-     var dd = parseInt(dds);
+var dateWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-     var yearc = yys.split("");
-     var cc = yearc[0].concat(yearc[1]);
-     var yy = yearc[2].concat(yearc[3]);
-     var cc = parseInt(cc);
-     var yy = parseInt(yy);
-     fd = Math.round( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+function getuserInfo(){
+var day = parseInt(document.getElementById("day").value);
 
-     if(!sex){
-       alert('No sex was selected. Try again.');
-       return false;
-     }
-     else if(sex === 'Female' && fd === 0){
-       alert("Akosua");
-     }
-     else if(sex === 'Male' && fd === 0){
-         alert("Kwasi");
-     }
-     else if(sex === 'Female' && fd === 1){
-       alert("Adwoa");
-     }
-     else if(sex === 'Male' && fd === 1){
-         alert("Kwadwo");
-     }
-     else if(sex === 'Female' && fd === 2){
-       alert("Abenaa");
-     }
-     else if(sex === 'Male' && fd === 2){
-         alert("Kwabena");
-     }
-     else if(sex === 'Female' && fd === 3){
-       alert("Akua");
-     }
-     else if(sex === 'Male' && fd === 3){
-         alert("Kwaku");
-     }
-     else if(sex === 'Female' && fd === 4){
-       alert("Yaa");
-     }
-     else if(sex === 'Male' && fd === 4){
-         alert("Yaw");
-     }
-     else if(sex === 'Female' && fd === 5){
-       alert("Afua");
-     }
-     else if(sex === 'Male' && fd === 5){
-         alert("Kofi");
-     }
-     else if(sex === 'Female' && fd === 6){
-       alert("Ama");
-     }
-     else if(sex === 'Male' && fd === 6){
-         alert("Kwame");
-     }else{
-         alert("Try Again");
-     }
+var month = parseInt(document.getElementById("month").value);
 
+var year = parseInt(document.getElementById("year").value);
 
-     }
+var gender = radioInfo();
 
-         </script>
-Message Input
+var dayWeek = new Date(year + "/" + month + "/" + day);
 
+var d = dayWeek.getDay();
 
-Message
+var name="" ;
+
+if (gender ==="male"){
+   name = male[d];
+}
+
+else{
+   name = female[d];
+}
+
+alert("You were born on "+ dateWeek[d] + " and your Akan name is "+ name);
+}
+
+function radioInfo (){
+var radio = document.getElementsByName('gender');
+
+for (var i = 0;  i<radio.length; i++)
+{
+if (radio[i].checked)
+{
+ // do whatever you want with the checked radio
+ gender = radio[i].value;
+
+ // only one radio can be logically checked, don't check the rest
+ return gender;
+}
+}
+
+}
+
+// function calculateDay(){
+
+// }
